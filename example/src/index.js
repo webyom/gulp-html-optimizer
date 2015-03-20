87db1b06b1e4b857e313d5a42d94b3c7df6d595f
webyom@gmail.com <webyom@gmail.com>
2015-03-20 13:25:37 +0800

    0.1.4

--- example/src/index.js
+++ example/src/index.js
@@ -1,5 +1,15 @@
 define(['require', 'exports', 'module', './mod-a'], function(require, exports, module, modA) {
 	var modB = require('./mod-b');
+	var tplA = require('./inline-tpl-a.tpl.html');
+	var tplB = require('./inline-tpl-b.tpl.html');
 
 	return {};
 });
+
+__END__
+
+@@ inline-tpl-a.tpl.html
+<div>A</div>
+
+@@ inline-tpl-b.tpl.html
+<div>B</div>
